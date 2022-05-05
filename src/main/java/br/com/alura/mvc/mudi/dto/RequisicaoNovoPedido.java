fd66f63ff56cc6018bcc2ec1e147d382134c4486
Rogerio Prando <rogerio.prando@wplex.com.br>
2022-05-05 18:25:53 -0300

    api rest

--- src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -1,14 +1,12 @@
 package br.com.alura.mvc.mudi.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
 
-import javax.validation.constraints.NotBlank;
-
 public class RequisicaoNovoPedido {
 
-    // Em caso de erro retorna
-    // NotBlank.requisicaoNovoPedido.nomeProduto=
     @NotBlank
     private String nomeProduto;
 
@@ -17,50 +15,42 @@ public class RequisicaoNovoPedido {
 
     @NotBlank
     private String urlImagem;
-
     private String descricao;
 
     public String getNomeProduto() {
         return nomeProduto;
     }
-
     public void setNomeProduto(String nomeProduto) {
         this.nomeProduto = nomeProduto;
     }
-
     public String getUrlProduto() {
         return urlProduto;
     }
-
     public void setUrlProduto(String urlProduto) {
         this.urlProduto = urlProduto;
     }
-
     public String getUrlImagem() {
         return urlImagem;
     }
-
     public void setUrlImagem(String urlImagem) {
         this.urlImagem = urlImagem;
     }
-
     public String getDescricao() {
         return descricao;
     }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
     public Pedido toPedido() {
         Pedido pedido = new Pedido();
-        pedido.setDescricao(this.descricao);
-        pedido.setNomeProduto(this.nomeProduto);
-        pedido.setUrlImagem(this.urlImagem);
-        pedido.setUrlProduto(this.urlProduto);
+        pedido.setDescricao(descricao);
+        pedido.setNomeProduto(nomeProduto);
+        pedido.setUrlImagem(urlImagem);
+        pedido.setUrlProduto(urlProduto);
         pedido.setStatus(StatusPedido.AGUARDANDO);
         return pedido;
     }
+
+
 }
-
-
--- src/main/java/br/com/alura/mvc/mudi/model/User.java
+++ src/main/java/br/com/alura/mvc/mudi/model/User.java
@@ -1,16 +1,22 @@
 package br.com.alura.mvc.mudi.model;
 
-import javax.persistence.*;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 @Entity
-@Table(name = "users")
+@Table(name="users")
 public class User {
 
     @Id
     private String username;
     private String password;
-    private boolean enabled;
+    private Boolean enabled;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user", fetch = FetchType.LAZY)
     private List<Pedido> pedidos;
@@ -18,32 +24,21 @@ public class User {
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
-    public boolean isEnabled() {
+    public Boolean getEnabled() {
         return enabled;
     }
-
-    public void setEnabled(boolean enabled) {
+    public void setEnabled(Boolean enabled) {
         this.enabled = enabled;
     }
 
-    public List<Pedido> getPedidos() {
-        return pedidos;
-    }
 
-    public void setPedidos(List<Pedido> pedidos) {
-        this.pedidos = pedidos;
-    }
 }
